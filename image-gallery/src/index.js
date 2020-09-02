eaf9e1fea4d296c8a390226d97f3f9a2f2ba8433
Guilherme Popolin <guilhermespopolin@gmail.com>
2020-09-03 01:47:39 +0200

    wip: addd panels

--- image-gallery/src/index.js
+++ image-gallery/src/index.js
@@ -1,13 +1,41 @@
 const panels = document.querySelectorAll(".gallery__panel");
 
 function toggleOpen() {
-  this.classList.toggle("gallery__panel--open");
+  const isAlreadyOpened = this.classList.contains('gallery__panel--open');
+  
+  if (isAlreadyOpened) {
+    this.classList.remove('gallery__panel--open');
+  } else {
+    const currentOpenedPanel = this.parentNode.querySelector('.gallery__panel--open');
+    
+    if (currentOpenedPanel) {
+      currentOpenedPanel.classList.remove('gallery__panel--open');
+    }
+
+    this.classList.add('gallery__panel--open');
+  }
 }
 
-function toggleActive() {
-  this.classList.toggle("gallery__panel--active");
+function activate(event) {
+  const isPanelOpen = this.classList.contains('gallery__panel--open');
+  const doesPanelFinishResizing = event.propertyName === 'flex-grow';
+
+  if (isPanelOpen && doesPanelFinishResizing) {
+    this.classList.add('gallery__panel--active');
+  }
 }
 
-panels.forEach((imageGallery) => {
-  imageGallery.addEventListener("click", toggleOpen);
+function deactivate(event) {
+  const isPanelClosed = !this.classList.contains('gallery__panel--open');
+  const doesPanelStartResizing = event.propertyName === 'flex-grow';
+  
+  if (isPanelClosed && doesPanelStartResizing) {
+    this.classList.remove('gallery__panel--active');
+  }
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', toggleOpen);
+  panel.addEventListener('transitionstart', deactivate);
+  panel.addEventListener('transitionend', activate);
 });
